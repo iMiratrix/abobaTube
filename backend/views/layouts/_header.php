73a2ff8202194d52c16da93b55576d3eaf6ced37
Abobus <imiratrix@yahoo.com>
2022-03-15 04:08:04 +0400

    first commit

---
+++ backend/views/layouts/_header.php
@@ -0,0 +1,36 @@
+<header>
+    <?php
+
+    use yii\bootstrap4\Html;
+    use yii\bootstrap4\Nav;
+    use yii\bootstrap4\NavBar;
+
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar navbar-expand-lg navbar-light bg-light shadow-sm',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => 'Home', 'url' => ['/site/index']],
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = '<li>'
+            . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
+            . Html::submitButton(
+                'Logout (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']
+            )
+            . Html::endForm()
+            . '</li>';
+    }
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
+</header>
